7f8f11ba19f9dd0e95367e529139e896f43f4239
Remzi <52746405+FreedreamGameStudio@users.noreply.github.com>
2024-11-20 23:07:51 +0300

    Create http_request_manager.h

---
+++ http_request_manager.h
@@ -0,0 +1,51 @@
+/*
+ * This file constructed by Remzi ISCI
+ *        11/20/2024
+ *
+ * This file header file for simple http request manager without using any library.
+ * Each request will be handled in a separate thread.
+ * Request does not use ssl. If you want to use ssl, you need to use openssl or other library like curl.
+ * You can this codes freely even in commercial projects, it will be kind if you mention me in your project.
+ * For use on windows uncomment define WIN32
+ */
+
+
+
+#ifndef FATIO_HTTP_REQUEST_MANAGER_H
+#define FATIO_HTTP_REQUEST_MANAGER_H
+
+/**
+ * Server root address
+ * set your server address here
+ */
+#define Server "httpbin.org"
+
+/**
+ * Server port, the most commonly 80 for http
+ */
+#define PORT 80
+
+
+/**
+ * Callback function type that will be called when the response is received
+ * @param response The response from the server
+ *
+ * simple callback function that takes a string as a parameter holds response data
+ *
+ *   void handle_response(const char *response) {
+ *     printf("HTTP Response: %s\n", response);
+ *   }
+ */
+typedef void (*ResponseCallback)(const char *);
+
+
+
+/**
+ * Send a HTTP POST request to the server
+ * @param callback
+ * @param data to send forexaample "name=remzi&age=25"
+ * @param post_path path to send post request for example "/user.php"
+ */
+ void send_http_post_request(ResponseCallback callback, const char *data, const char  *post_path);
+
+#endif // FATIO_HTTP_REQUEST_MANAGER_H
